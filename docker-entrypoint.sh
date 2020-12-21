9ab35b48089e6a3eb48a4d03fd3df16853eae136
nicobo <nicobo@users.noreply.github.com>
2020-12-21 23:34:46 +0100

    + installing signal dependencies + option to link to a device when running the docker image (alpine only for now)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 usage()
 {
@@ -13,16 +13,46 @@ E.g. '$0 transbot -h' to get a more specific help for 'transbot'
 EOF
 }
 
-# It's not needed to repeat the commands for each bot but it's clearer
-bot=$1
-case $bot in
-    transbot)
-        shift
-        exec python -m "nicobot.$bot" "$@"
-        ;;
-    askbot)
-        shift
-        exec python -m "nicobot.$bot" "$@"
+
+# Displays an URL and a QRCode in the console to link the current container
+# with whichever Signal client will scan it
+signal_link() {
+    device_name=$1
+    # WARNING This command works on alpine with bash installed, not tested otherwise
+    signal-cli link --name "${device_name}" | tee >(head -1 | qr)
+}
+
+# Default values
+opt_signal_register=
+opt_bot=
+
+# Parses the command line for options to execute before running the bot
+# TODO Enhance CLI handling with getopt
+#ARGS=$(getopt -l 'signal-register:' -- "$@") || usage && exit 1
+#eval "set -- $ARGS"
+while true; do
+    case $1 in
+      (--signal-register)
+            opt_signal_register=$2
+            shift 2;;
+      (askbot|transbot)
+            opt_bot=$1
+            shift;;
+      (*)
+            # End of this script's options ; next options are for the bot
+            break;;
+    esac
+done
+
+# Registers the device with signal
+if [ -n "${opt_signal_register}" ]; then
+    signal_link "${opt_signal_register}"
+fi
+
+# Runs the right bot with the remaining args
+case "${opt_bot}" in
+    askbot|transbot)
+        exec python3 -m "nicobot.${opt_bot}" "$@"
         ;;
     *)
         usage
